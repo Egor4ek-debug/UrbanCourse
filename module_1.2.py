0951a530cdbda431902f459a77a3e9d326afffdf
Egor <nabatov.egor@gmail.com>
2024-08-01 17:40:55 +0300

    Add a tasl 1.2

---
+++ module_1.2.py
@@ -0,0 +1,10 @@
+countDoneHomework = 12
+
+countHours = 1.5
+
+course = 'Python'
+
+time = countHours/countDoneHomework
+
+print(f"Курс: {course}, всего задач:{countDoneHomework}, затрачено часов:{countHours},среднее время выполнения {time} часа")
+
