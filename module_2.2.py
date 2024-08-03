8573a1abb82cd02606da5b238f690157fc2e3634
Egor <nabatov.egor@gmail.com>
2024-08-03 22:58:56 +0300

    add a module 2.2

---
+++ module_2.2.py
@@ -0,0 +1,12 @@
+first = int(input('Enter you first digital '))
+
+second = int(input('Enter you second digital '))
+
+third = int(input('Enter you third  digital '))
+
+if first == second ==third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
