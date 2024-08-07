195dd2368e3640b74e6e88d7a6e9240e2a5d34ba
Egor <nabatov.egor@gmail.com>
2024-08-07 23:07:40 +0300

    add a module 3.2

--- module_3.2.py
+++ module_3.2.py
@@ -1,13 +1,11 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
     valid_domains = ('.com', '.ru', '.net')
 
-    def verify_email(email):
+    def is_valid_email(email):
         return '@' in email and str(email).endswith(valid_domains)
 
-    if verify_email(sender) and verify_email(recipient):
-        pass
-    else:
-        return f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}'
+    if not is_valid_email(sender) or not is_valid_email(recipient):
+        return f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}"
     if sender == recipient:
         return 'Нельзя отправить письмо самому себе!'
     if sender == 'university.help@gmail.com':
@@ -19,3 +17,17 @@ print(send_email('Это сообщение для проверки связи',
 print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com'))
 print(send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk'))
 print(send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
+
+
+# def send_mail(message, recipient, sender='university.help@gmail.com'):
+#     valid_domains = ('.com', '.ru', '.net')
+#
+#     def is_valid_email(email):
+#         return '@' in email and str(email).endswith(valid_domains)
+#
+#     if not is_valid_email(sender) or not is_valid_email(recipient):
+#         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+#
+#     if recipient == sender:
+#         print('Нельзя отправить письмо самому себе!')
+
