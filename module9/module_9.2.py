246f93ea75b6dd0c6bc8c1cbdea0cfbe18e97e2e
Egor4ek-debug <egornabatov4@gmail.com>
2024-09-09 22:28:34 +0300

    add a module 9.2

--- module9/module_9.2.py
+++ module9/module_9.2.py
@@ -7,7 +7,7 @@ first_result = [len(string) for string in first_strings if len(string) >= 5]
 second_result = [(first_string, second_string) for first_string in first_strings for second_string in second_strings if
                  len(first_string) == len(second_string)]
 combined_list = first_strings + second_strings
-third_result = [{name: len(name)} for name in combined_list if len(name) % 2 == 0]
+third_result = {name: len(name) for name in combined_list if len(name) % 2 == 0}
 
 print(first_result)
 print(second_result)
