809723ef84d88e4712e0443842f8d08483f453bd
Egor <nabatov.egor@gmail.com>
2024-11-03 18:25:13 +0300

    add for module 14.1

--- module14/module_14_1.py
+++ module14/module_14_1.py
@@ -15,24 +15,24 @@ cursor.execute('''
 
 cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
 
-# for i in range(10):
-#     cursor.execute('INSERT INTO Users(username,email,age,balance) VALUES(?,?,?,?)',
-#                    (f'User{i + 1}', f'example{i + 1}@gmail.com', (i + 1) * 10, 1000))
+for i in range(10):
+    cursor.execute('INSERT INTO Users(username,email,age,balance) VALUES(?,?,?,?)',
+                   (f'User{i + 1}', f'example{i + 1}@gmail.com', (i + 1) * 10, 1000))
 
-# cursor.execute('''
-#     UPDATE Users
-#     SET balance = 500
-#     WHERE id IN(
-#         SELECT id FROM Users WHERE id % 2 = 1
-#     )
-# ''')
+cursor.execute('''
+    UPDATE Users
+    SET balance = 500
+    WHERE id IN(
+        SELECT id FROM Users WHERE id % 2 = 1
+    )
+''')
 
-# cursor.execute("SELECT * FROM Users ORDER BY id")
-# ids = [row[0] for row in cursor.fetchall()]
-#
-# ids_for_delete = [id for index, id in enumerate(ids) if (index + 1) % 3 == 0]
-#
-# cursor.executemany("DELETE FROM Users WHERE id = ?", [(id,) for id in ids_for_delete])
+cursor.execute("SELECT * FROM Users ORDER BY id")
+ids = [row[0] for row in cursor.fetchall()]
+
+ids_for_delete = [id for index, id in enumerate(ids) if (index + 1) % 3 == 0]
+
+cursor.executemany("DELETE FROM Users WHERE id = ?", [(id,) for id in ids_for_delete])
 
 cursor.execute("SELECT username,email,age,balance FROM Users WHERE age != 60")
 records = cursor.fetchall()
